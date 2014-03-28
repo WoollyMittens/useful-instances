5ee3b8802088273b1f613ef1bcf1cd5f648576fb
Maurice van Creij <maurice.vancreij@webqem.com>
2014-03-28 16:18:31 +1100

    Updated documentation.

--- inc/js/useful-instances.js
+++ inc/js/useful-instances.js
@@ -76,7 +76,8 @@
 
 	// allow console.log
 	polyfills.consoleLog = function () {
-		if (!window.console) {
+		var overrideTest = new RegExp('console-log', 'i');
+		if (!window.console || overrideTest.test(document.querySelectorAll('html')[0].className)) {
 			window.console = {};
 			window.console.log = function () {
 				// if the reporting panel doesn't exist
@@ -107,6 +108,8 @@
 				for (a = 0, b = arguments.length; a < b; a += 1) {
 					messages += arguments[a] + '<br/>';
 				}
+				// add a break after the message
+				messages += '<hr/>';
 				// output the queue to the panel
 				reportPanel.innerHTML = messages + reportString;
 			};
@@ -189,8 +192,10 @@
 			for (var a = 0, b = this.objs.length; a < b; a += 1) {
 				// store a constructed instance with cloned cfgs object
 				this.constructs[a] = new this.constructor(this.objs[a], Object.create(this.cfgs));
-				this.constructs[a].start();
 			}
+			// disable the start function so it can't be started twice
+			this.start = function () {};
+			// empty the timeout
 			return null;
 		};
 		// returns the constructs
@@ -205,6 +210,8 @@
 		this.getByIndex = function (index) {
 			return this.constructs[index];
 		};
+		// go
+		this.wait();
 	};
 
 }(window.useful = window.useful || {}));
